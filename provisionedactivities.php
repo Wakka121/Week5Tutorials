72f9665ab96b636c72c1d95c51a7913a063a1a48
Wakka121 <1615091@rgu.ac.uk>
2017-02-28 16:19:12 +0000

    Added provisionedactivities.php

---
+++ provisionedactivities.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1615091
+ * Date: 28/02/2017
+ * Time: 16:16
+ */
+$provisionedActivities = array("Specs", "Mugs", "Sausage Rolls");
+
+foreach($provisionedActivities as $x) {
+    echo $x;
+}
