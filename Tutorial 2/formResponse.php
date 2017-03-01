0339902b47c28298e67c4ab65688d13db080a8e9
Wakka121 <1615091@rgu.ac.uk>
2017-03-01 15:00:52 +0000

    Form post input and response

---
+++ Tutorial 2/formResponse.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wakka
+ * Date: 01/03/2017
+ * Time: 14:59
+ */
+$forename = $_POST["forename"];
+$surname = $_POST["surname"];
+echo $forename . $surname;
