04907390707e744839d945f0d3ca29aa5506039e
Wakka121 <1615091@rgu.ac.uk>
2017-03-01 15:21:21 +0000

    new superheroes

--- Tutorial 2/newsuperhero.php
+++ Tutorial 2/newsuperhero.php
@@ -6,14 +6,9 @@
 <body>
 <?php
 if ($_SERVER["REQUEST_METHOD"]==="GET"){
-    echo "<form action="superheroDisplay.php" method="post">"
-    echo "<label>Forename</label><input type="text" name="forename"><br>"
-    echo "<label>Surname</label><input type="text" name="surname"><br>"
-    echo "<label>Gender</label><input type="text" name="gender"><br>"
-    echo "<label>Power</label><input type="text" name="power">"
-    echo "<input type="submit" value="Submit">"
-    echo "</form>"
+    include "superheroCreator.html";
 }
+
 ?>
 </body>
 </html>
