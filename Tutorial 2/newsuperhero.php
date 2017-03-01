30aceceaf50280dbe52a93fa21cb478dd56f34d6
Wakka121 <1615091@rgu.ac.uk>
2017-03-01 15:23:23 +0000

    new superheroes

--- Tutorial 2/newsuperhero.php
+++ Tutorial 2/newsuperhero.php
@@ -8,6 +8,16 @@
 if ($_SERVER["REQUEST_METHOD"]==="GET"){
     include "superheroCreator.html";
 }
+elseif ($_SERVER["REQUEST_METHOD"]==="POST"){
+    $forename = $_POST["forename"];
+    $surname = $_POST["surname"];
+    $gender = $_POST["gender"];
+    $power = $_POST["power"];
+
+    echo "Name: " . $forename . " " . $surname . "<br>";
+    echo "Gender: " . $gender . "<br>";
+    echo "Power: " . $power . "<br>";
+}
 
 ?>
 </body>
