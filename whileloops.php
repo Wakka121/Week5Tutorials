98098e59f2cbd729f1f894b14386b37976c9c9db
Wakka121 <1615091@rgu.ac.uk>
2017-03-01 13:44:07 +0000

    Added page on while loops

---
+++ whileloops.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wakka
+ * Date: 01/03/2017
+ * Time: 13:32
+ */
+$day = 1;
+$inv = array(7,7,7);
+$goods = array("specs","mugs","sausage rolls");
+
+while($inv[0]>0 and $inv[1]>0 and $inv[2]>0){
+    $todaysGood = rand(0,2);
+
+    print "On day $day of the month $inv[$todaysGood] are available";
+    $inv[$todaysGood] = $inv[$todaysGood]-1;
+}
+
+print "No more goods are available this month";
